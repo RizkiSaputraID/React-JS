f22809a7ceaf67d0f6a2af1cc0f21f55ab4d975a
Rizki Firnanda Saputra <63184749+rizkipare12@users.noreply.github.com>
2021-09-22 12:12:23 +0700

    Add files via upload

---
+++ recipe-app-native-main/src/components/search-bar.js
@@ -0,0 +1,31 @@
+import { search } from './fontAwesome.js'
+class SearchBar extends HTMLElement {
+  connectedCallback () {
+    this.render()
+  }
+
+  // eslint-disable-next-line accessor-pairs
+  set eventSubmit (event) {
+    this._eventSubmit = event
+  }
+
+  get valueRecipe () {
+    return this.querySelector('.search__input').value
+  }
+
+  render () {
+    this.innerHTML = `
+    <div class="search">
+      <form class="search__form">
+        <input class="search__input" type="text" placeholder="cari resepmu disini" required>
+        <button class="search__btn">${search}</button>
+      </form>
+    </div>
+    `
+    this.querySelector('.search__btn').addEventListener('click', (e) => {
+      this._eventSubmit(e)
+    })
+  }
+}
+
+customElements.define('search-bar', SearchBar)
